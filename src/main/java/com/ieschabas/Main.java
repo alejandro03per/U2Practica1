5793d372b1ac72ed97df4280b6ad2b26255da206
aleperser4 <aleperser4@alu.edu.gva.es>
2025-09-19 16:52:16 +0200

    Objeto de la clase

--- src/main/java/com/ieschabas/Main.java
+++ src/main/java/com/ieschabas/Main.java
@@ -1,9 +1,33 @@
 package com.ieschabas;
 
+import java.net.SocketOption;
+import java.security.spec.RSAOtherPrimeInfo;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Instanciamos un objeto persona
+        Persona persona1 = new Persona();
+        //Hemos añadido datos al objeto persona1
+        persona1.setNombre("Alejandro");
+        persona1.setApellidos("Perez Serra");
+        persona1.setDni("55555555E");
+        persona1.setEdad(27);
+        persona1.setTelefono("55555555");
+        //Imprimimos los datos por consola
+        System.out.println(persona1.toString());
+
+        //Instanciamos un objeto Persona con constructor sobrecargado
+        Persona persona2 = new Persona(22, "5555555L", "Alicia", "FFF", "+34478745837");
+        System.out.println(persona2.toString());
+
+        //Cambiar la edad a 50 e imprimir el objeto
+        persona1.setEdad(50);
+        //Comprobamos que la edad se ha establecido
+        System.out.println("Nueva edad: " + persona1.getEdad());
+        //Imprimimos todo el objeto
+        System.out.println(persona1.toString());
 
 
     }
---
+++ src/main/java/com/ieschabas/Persona.java
@@ -0,0 +1,135 @@
+package com.ieschabas;
+
+/**
+ * La clase persona define un objeto de tipo persona con atributos como,
+ * nombre,apellidos,edad,dni y telefono
+ * @author Alejandro
+ * @version 1.0
+ */
+public class Persona {
+    //Atributos de la clase
+    private int edad;
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+
+    //Constructores
+
+    /**
+     * Constructor por defecto crea una instancia de persona sin datos
+     */
+
+    public Persona(){
+    }
+    public Persona(int edad, String dni, String nombre, String apellidos, String telefono){
+        this.edad=edad;
+        this.dni= dni;
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.telefono=telefono;
+    }
+
+    //Setters
+
+    /**
+     * Inserta la edad sobre el objeto pesona
+     * @param edad
+     */
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    /**
+     * Inserta DNI sobre objeto persona
+     * @param dni
+     */
+    public void setDni(String dni){
+        this.dni=dni;
+    }
+
+    /**
+     * Insert el nombre sobre objeto persona
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Inserta apellidos sobre objeto persona
+     * @param apellidos
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * Inserta el telefono sobre el objeto persona
+     * @param telefono
+     */
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+
+    //Getters
+
+    /**
+     * Devuelve edad sobre objeto persona
+     * @return edad
+     */
+    public int getEdad() {
+        return this.edad;
+    }
+
+    /**
+     * Devuelve el DNI sobre objeto persona
+     * @return dni
+     */
+    public String getDni() {
+        return this.dni;
+    }
+
+    /**
+     * Devuelve el nombre sobre objeto persona
+     * @return nombre
+     */
+    public String  getNombre() {
+        return this.nombre;
+    }
+
+    /**-
+     * Devuelve los apellidos sobre objeto persona
+     * @return apellidos
+     */
+    public String getApellidos(){
+        return this.apellidos;
+    }
+    /**
+     * Devuelve telefono sobre objetos personas
+     */
+    public String getTelefono(){
+        return this.telefono;
+    }
+
+    /**
+     * Constructores sobrecargando de la clñase Persona, crea
+     * @return
+     */
+
+    //Método que imprime el objeto
+
+    /**
+     * devuelve el objet personas en formato cadena
+     * @return cadena con los valores de persona
+     */
+    public String toString (){
+        return "Nobre " + this.nombre + "\nApellidos: " + this.apellidos + "\nEdad: " + this.edad + "\nDNI: " + this.dni + "\nTelefono: " + this.telefono;
+
+
+
+
+
+    }
+}
